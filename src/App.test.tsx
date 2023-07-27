a18f91c23ea0d735cb76ac547fdfdfb0636c6e2b
kcuevas <cuev73@live.com>
2023-07-27 08:32:09 -0400

    intial unit tests for App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,20 @@
 import {render, screen} from "@testing-library/react";
-import {describe,expect, it } from "vitest"
 import App from "./App"
 
+
 describe("App Component", () => {
+  
   it("should have text hello world", ()=> {
     render(<App />);
     const message = screen.queryByText(/Hello World/i);
     expect(message).toBeVisible();
-  })
+  });
+
+  it("should have an h1 heading with hello world rendered", () => {
+    render(<App />);
+    const heading = screen.getByRole("heading", {level: 1})
+    expect(heading).toHaveTextContent(/hello world/i);
+  });
+
 })
+
